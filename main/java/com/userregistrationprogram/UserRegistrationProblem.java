27353795bf8ebd8eb0410cbfec81dae29ffede94
prudhvi <prudhvieethakota123@gmail.com>
2021-05-16 01:57:37 +0530

    Added: Welcome Message to User Registration Problem

---
+++ main/java/com/userregistrationprogram/UserRegistrationProblem.java
@@ -0,0 +1,11 @@
+package com.userregistrationprogram;
+
+/**
+ * @description Class for defining the User Registration with regex
+ *
+ */
+public class UserRegistrationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Program"); //Printing the Welcome Message
+    }
+}
